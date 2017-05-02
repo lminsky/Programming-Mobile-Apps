b6c8ab4a2e4853dfce546cfaef3d6b166383c5bd
Louis Minsky <lminsky@packer.edu>
2017-05-02 10:06:02 -0400

    added comments

--- Class Examples/2017-05-02/HTTP Get/main.lua
+++ Class Examples/2017-05-02/HTTP Get/main.lua
@@ -1,14 +1,15 @@
-local json = require( "json" )
+local json = require( "json" )    -- Bring in the JSON library to convert JSON to a Lua Table
 
 function getZip(event)
-    for k,v in pairs(event) do
-        print(k,v)
-    end
-
-    -- if(event.status == 200) then
-    --     print(event.response)
-    --     local response = json.decode(event.response)
+    -- for k,v in pairs(event) do
+    --     print(k,v)
     -- end
+
+    if(event.status == 200) then    --if the HTTP response has a status code of 200 (OK), proceed
+        -- print(event.response)
+        local response = json.decode(event.response)  -- turn the JSON response into a Lua table
+        print(response.zip_code)      -- Print out the zip code as determined by the response
+    end
 end
 
-network.request( "http://freegeoip.net/json/", "GET", getZip )
+network.request( "http://freegeoip.net/json/", "GET", getZip )    -- Make an http GET request to the specified website. When there is a response, call the get zip function
